5567cdc44b1fd76f157af70078b25937231c3f06
Lee <espinosalee123@gmail.com>
2023-04-22 09:36:25 -0700

    Save budget

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import streamlit as st
 import pandas as pd
+import os.path
 
 # Define the layout of your app
 st.title("Budget App")
 
 # Create input forms
 col1, col2 = st.columns(2)
-monthly_income = col1.number_input("Monthly Income", min_value=0, step=50)
-expenses = col2.number_input("Monthly Expenses", min_value=0, step=50)
+monthly_income = col1.number_input("Monthly Income", min_value=0, step=50, key="monthly_income")
+expenses = col2.number_input("Monthly Expenses", min_value=0, step=50, key="expenses")
+
 
 # Add data processing functionality
-savings = monthly_income - expenses
-if savings < 0:
+if expenses > monthly_income:
     st.warning("Your expenses are greater than your income. You need to cut back on expenses.")
 else:
-    st.success("You are saving ${} every month.".format(savings))
+    st.success("You are saving ${} every month.".format(monthly_income - expenses))
 
 # Create output visuals
 st.header("Monthly Expenses")
 
 # Get user input for expense names and amounts
-num_expenses = st.number_input("How many expenses do you have?", min_value=0, step=1)
+num_expenses = st.number_input("How many expenses do you have?", min_value=0, step=1, key="num_expenses")
 expense_names = []
 expense_amounts = []
 for i in range(num_expenses):
@@ -39,11 +40,47 @@ expense_data = pd.concat([expense_data, total_expenses], ignore_index=True)
 # Display the expense data in a table
 st.table(expense_data)
 
+# Check if any expenses are over budget
+if total_expenses["Amount"][0] > expenses:
+    st.warning("Your expenses are over budget.")
+else:
+    st.success("Your expenses are within budget.")
+
+# Check if the file containing the previous budget data exists
+if os.path.isfile("budget_data.csv"):
+    budget_data = pd.read_csv("budget_data.csv")
+else:
+    budget_data = pd.DataFrame(columns=["Monthly Income", "Expenses", "Savings"])
+
+# Add current budget data to DataFrame
+current_budget_data = pd.DataFrame({"Monthly Income": monthly_income, "Expenses": expenses, "Savings": monthly_income - expenses}, index=[0])
+budget_data = budget_data.append(current_budget_data, ignore_index=True)
+
+# Save budget data to file
+budget_data.to_csv("budget_data.csv", index=False)
+
+# Display previous budget data in a table
+st.header("Previous Budget Data")
+st.table(budget_data)
+
+# Check if the file containing the previous expense data exists
+if os.path.isfile("expense_data.csv"):
+    prev_expense_data = pd.read_csv("expense_data.csv")
+else:
+    prev_expense_data = pd.DataFrame(columns=["Expense Name", "Amount"])
+
+# Append current expense data to DataFrame
+current_expense_data = pd.DataFrame({"Expense Name": expense_names, "Amount": expense_amounts})
+expense_data = pd.concat([prev_expense_data, current_expense_data], ignore_index=True)
+
+# Save expense data to file
+expense_data.to_csv("expense_data.csv", index=False)
+
 
 # Deploy your app
 # Use the command `streamlit run app.py` to run your app locally
 # You can also deploy your app to hosting services like Heroku or Streamlit Sharing
 
 
-#Todo:
-"""Get the table to start from '1' instead of '0' """
+
+
